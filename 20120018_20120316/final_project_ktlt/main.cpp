3d6529beb1ec896bb92c9f8df60843d54f565fc5
minhlam2102002 <minhlam2102002@gmail.com>
2021-06-13 13:11:19 +0700

    update read folder

--- 20120018_20120316/final_project_ktlt/main.cpp
+++ 20120018_20120316/final_project_ktlt/main.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 #include<string>
-using namespace std;
+#include<vector>
+#include <filesystem>
+
+using namespace std; 
+namespace fs = std::filesystem;
+
 
 void Search(string input) {
 
 }
-void Print(char*)
+string extractFileName(string path) {
+	string res;
+	for (int i = path.size() - 1; path[i] != '\\'; i--) {
+		res += path[i];
+	}
+	reverse(res.begin(), res.end());
+	return res;
+}
+void createIndexFile(string path) {
+	vector<string> topic;
+	for (auto& file : fs::directory_iterator(path)) {
+		fs::path p = file.path();
+		topic.push_back(extractFileName(p.string()));
+	}
+	for (string file : topic) {
+		cout << file << endl;
+	}
+}
+void createMetadata(string path) {
+
+}
 int main()
 {
-	string input;
-	getline(cin, input);
-	Search(input);
-	Print();
+	string root = "C:\\Users\\MINH LAM\\source\\repos\\20120018_20120316\\final_project_ktlt\\source";
+	string trainPath = root + "\\Train\\new train";
+	createIndexFile(trainPath);
 }
