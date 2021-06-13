ab33424001505b8d48bb06920d24584d7de6147a
Tran Kieu Minh Lam <31835490+minhlam2102002@users.noreply.github.com>
2021-06-13 09:50:41 +0700

    Upload project

---
+++ 20120018_20120316/final_project_ktlt/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void Search(string input) {
+
+}
+void Print(char*)
+int main()
+{
+	string input;
+	getline(cin, input);
+	Search(input);
+	Print();
+}
