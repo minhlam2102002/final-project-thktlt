8f2897d2fac498d7b07bb92e60724485dc1be6be
minhlam2102002 <minhlam2102002@gmail.com>
2021-06-21 15:11:05 +0700

    xoa dau, lower case, read stopword

--- 20120018_20120316/final_project_ktlt/main.cpp
+++ 20120018_20120316/final_project_ktlt/main.cpp
@@ -1,4 +1,4 @@
-#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+﻿#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 #define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS 
 #define _CRT_SECURE_NO_WARNINGS_
 
@@ -11,6 +11,8 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <sstream>
+#include <regex>
 #include "InOut.h"
 
 using namespace std;
@@ -60,7 +62,69 @@ void createMetadata(std::wstring path, std::wstring topic) {
 //}
 //out.close();
 }
+void loverCase(string &str){
+    for(int i =0;i<strlen(str.c_str());i++){
+        str[i] = tolower(str[i]);
+    }
+	// được xài hàm có sẵn khum đó :>
+}
+void fixWord(string &str){
+	for(int i =0;i < strlen(str.c_str);i++){
+		if(str[i] == "," || str[i] == "." || str[i] == "/" || str[i] == "?" 
+		|| str[i] == ":" || str[i] == ";"||str[i] == "-"||str[i] == "!" 
+		|| str[i] == "\"" || str[i] == "="){
+			// thay bang khoang trang de tach token
+			str[i] == " ";
+		}
+	}
+}
+void extractWord(string content, vector<string> &words) {
+	stringstream ss(content);
+	string word;
+	while (ss >> word) {
+		words.push_back(word);
+	}
+}
+bool deleteStopWord(vector<string> words, vector<string>& stopWords){
 
+}
+void createStopWord(vector<string> &stopWords, std::wstring path) {
+	std::wstring w_content = ReadWord(path);
+	string content = XoaDau(w_content);
+	char* tmp;
+	tmp = new char[content.size() + 1];
+	strcpy(tmp, content.c_str());
+	char* token = strtok(tmp, "\n");
+	string word(token);
+	stopWords.push_back(word);
+	while (token != NULL)
+	{
+		word = token;
+		stopWords.push_back(word);
+		printf("%s\n", token);
+		token = strtok(NULL, "\n");
+	}
+	delete[] tmp;
+}
+
+string XoaDau(std::wstring w_txt) {
+	w_txt = std::regex_replace(w_txt, std::wregex(L"à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|/g"), L"a");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|/g"), L"A");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|/g"), L"e");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|/g"), L"E");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"ì|í|ị|ỉ|ĩ|/g"), L"i");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"Ì|Í|Ị|Ỉ|Ĩ|/g"), L"I");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|/g"), L"o");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|/g"), L"O");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|/g"), L"u");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|/g"), L"U");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"ỳ|ý|ỵ|ỷ|ỹ|/g"), L"y");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"Ỳ|Ý|Ỵ|Ỷ|Ỹ|/g"), L"Y");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"đ"), L"d");
+	w_txt = std::regex_replace(w_txt, std::wregex(L"Đ"), L"D");
+	string s_txt(w_txt.begin(), w_txt.end());
+	return s_txt;
+}
 int main()
 {
 	InOut();
