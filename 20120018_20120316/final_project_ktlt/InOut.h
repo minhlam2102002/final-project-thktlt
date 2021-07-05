1676a9e301c875de54cb2aee0fb4782f5ad5e23b
Hoanglam1134 <64766759+Hoanglam1134@users.noreply.github.com>
2021-07-05 22:29:05 +0700

    Delete 20120018_20120316 directory

--- 20120018_20120316/final_project_ktlt/InOut.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void InOut();
-std::wstring ReadFileUTF16(std::string path);
-std::wstring ReadFileUTF8(std::string path);
-void PrintFileUTF8(std::string path, std::wstring content);
